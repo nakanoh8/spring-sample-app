caeadf8338fbcc787a20d9b2737d0cbd9d871e56
nakanoh <murry.xxx.7@gmail.com>
2020-04-17 07:15:48 +0900

    jQueryを導入してAjaxde

--- src/main/resources/static/js/sample.js
+++ src/main/resources/static/js/sample.js
@@ -5,26 +5,23 @@ function test(){
 function loadJson(){
     document.getElementById('iframe_id').setAttribute("src", "http://localhost:8080/GetWeather");
 
-    const json="test";
-    document.getElementById('p_id').setAttribute("value", json);
-
-    // $.ajax({
-    //     url:'http://localhost:8080/GetWeather',
-    //     type:'GET',
-    //     data:''
-    // })
-    // // Ajaxリクエストが成功した時発動
-    // .done( (data) => {
-    //     json = data;
-    //     console.log(data);
-    // })
-    // // Ajaxリクエストが失敗した時発動
-    // .fail( (data) => {
-    //     // $('.result').html(data);
-    //     console.log(data);
-    // })
-    // // Ajaxリクエストが成功・失敗どちらでも発動
-    // .always( (data) => {
-    //     console.log(data);
-    // });
+    $.ajax({
+        url:'http://localhost:8080/GetWeather',
+        type:'GET',
+        data:''
+    })
+    // Ajaxリクエストが成功した時発動
+    .done( (data) => {
+        $("#p_id").text(JSON.stringify(data));
+        console.log(data);
+    })
+    // Ajaxリクエストが失敗した時発動
+    .fail( (data) => {
+        // $('.result').html(data);
+        console.log(data);
+    })
+    // Ajaxリクエストが成功・失敗どちらでも発動
+    .always( (data) => {
+        console.log("リクエスト終了");
+    });
 }
